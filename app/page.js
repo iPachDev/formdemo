4d215aa8e6a383fe0e2bcb9cbb29b7a568de2b9c
Fernando Pacheco Martínez <fpacheco@bancobase.com>
2023-09-29 10:04:51 -0600

    +fix

--- app/page.js
+++ app/page.js
@@ -34,32 +34,32 @@ export default function Home() {
         <title>Innova Hub Group | Demo</title>
     </Head>
     <main
-     className="w-fll h-screen"
-     style={{background: '#ACE1D0'}}
+     className="w-fll min-h-screen"
+     style={{background: '#023047'}}
     >
-        <div className="flex">
-            <div className="w-1/2 h-screen ">
+        <div className="lg:flex text-white">
+            <div className="lg:w-1/2 h-screen">
                 <div className="h-screen flex items-center justify-center">
                     <div className="w-3/4">
                         <div className="text-4xl font-light">Innova Hub Group</div>
-                        <div className="text-gray-700">Innovación Digital</div>
-                        <div className="mt-10 text-justify">
+                        <div className="text-gray-300">Innovación Digital</div>
+                        <div className="mt-10 text-justify font-light">
                             En Innova Hub Group, nos especializamos en la creación de soluciones digitales para empresas y emprendedores.
-                            Y hoy queremos invitarte a conocer nuestro trabajo en el próximo evento en <label className="underline">Paseo de la Reforma 1230, Ciudad de México</label>.
+                            Y hoy queremos invitarte a conocer nuestro trabajo en el próximo evento en <label className="underline">Paseo de la Reforma 1230, Ciudad de México</label> el 10 de Septiembre 2023 a las 10:00 am.
                         </div>
                         <div className="mt-10 rounded-md">
-                            <iframe className="rounded-md" width="560" height="315" src="https://www.youtube.com/embed/HaqrLm4RMbY?si=liJ1LdnuTLrfsk2P" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            <iframe className="rounded-md lg:!w-full" width="560" height="315" src="https://www.youtube.com/embed/HaqrLm4RMbY?si=liJ1LdnuTLrfsk2P" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="w-1/2 h-screen">
+            <div className="lg:w-1/2 h-screen">
                 <div className="h-screen flex items-center justify-center">
-                    <div className="bg-white w-3/4 rounded-md p-6 drop-shadow-md bg-opacity-30 backdrop-blur-md shadow-lg">
+                    <div className="bg-white w-3/4 rounded-md p-6 drop-shadow-md bg-opacity-60 backdrop-blur-md shadow-lg">
                         <div
-                            className="text-1xl font-light text-center mb-8 text-gray-600"
+                            className="text-1xl font-light text-center mb-8 text-black"
                         >Registrate Gratis y asiste al mejor evento de transformación digital.</div>
-                        <div id='formhb'></div>
+                        <div id='formhb' className="!text-white"></div>
                     </div>
                 </div>
             </div>
--- app/success/page.js
+++ app/success/page.js
@@ -7,13 +7,13 @@ export default function page() {
     </Head>
     <main
      className="w-fll h-screen"
-     style={{background: '#ACE1D0'}}
+     style={{background: '#023047'}}
     >
         <div className="w-full h-screen">
             <div className="h-screen flex items-center justify-center">
-                <div className="bg-white w-3/4 rounded-md p-6 drop-shadow-md bg-opacity-30 backdrop-blur-md shadow-lg">
+                <div className="bg-black w-3/4 rounded-md p-6 drop-shadow-md bg-opacity-30 backdrop-blur-md shadow-lg">
                     <div
-                        className="text-1xl font-light text-center text-gray-600"
+                        className="text-1xl font-light text-center text-white"
                     >Gracias por registrarte, revisa en tu bandeja de entrada de tu correo la confirmación de tu registro, espera pronto mas noticias..</div>
                     <div className='mt-8'>
                         <img src='/img/check.png' className='w-20 mx-auto'/>
